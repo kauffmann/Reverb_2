20acb618d2cb49ea382adee9f3dd35fe99140f3f
Michael Kauffmann <kauffmann.michael@gmail.com>
2026-04-21 11:25:57 +0200

    Initial commit

---
+++ plugin/reverb/include/gui/Parameters.h
@@ -0,0 +1,13 @@
+
+
+
+#pragma once
+
+struct Parameters
+{
+    explicit  Parameters(){}
+
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Parameters)
+};
---
+++ plugin/reverb/src/gui/Parameters.cpp
@@ -0,0 +1,56 @@
+
+
+
+AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterLayout()
+{
+    AudioProcessorValueTreeState::ParameterLayout layout;
+    constexpr auto versionHint = 1;
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>(ParameterID{"SIZE",versionHint},
+        "Room Size",
+        juce::NormalisableRange<float>(10.0f, 200.0f, 10.0f),
+        50.0f, String(), AudioProcessorParameter::genericParameter,
+        [](float value, int) -> String
+        {
+            String valueToText =
+                String(value , 1) + " DODO";
+
+            return valueToText;
+        })); // default:
+
+
+    layout.add((std::make_unique<juce::AudioParameterFloat>(ParameterID{"DECAY",versionHint},
+        "Decay",
+        juce::NormalisableRange<float>(0.2f, 40.0f, 0.1f),
+        6.0f)));
+
+
+    layout.add((std::make_unique<juce::AudioParameterFloat>(ParameterID{"DRY", versionHint},
+        "Dry",
+        juce::NormalisableRange<float>(0.0f, 1.0f, 0.1f),
+        0.4f))); // default
+
+    layout.add((std::make_unique<juce::AudioParameterFloat>(ParameterID{"DIFFUSSER",versionHint},
+        "Diffusser Gain",
+        juce::NormalisableRange<float>(0.0f, 1.0f, 0.1f),
+        0.3f))); // default
+
+    layout.add((std::make_unique<juce::AudioParameterFloat>(ParameterID{"WET_REFLECTIONS",versionHint},
+        "Early Reflection Gain",
+        juce::NormalisableRange<float>(0.0f, 1.0f, 0.1f), 0.3f))); // default
+
+
+    layout.add((std::make_unique<juce::AudioParameterFloat>(ParameterID{"PREDELAY", versionHint},
+        "Predelay",
+        juce::NormalisableRange<float>(0.0f, 500.0f, 1.0f), 20.0f, String(), AudioProcessorParameter::genericParameter,
+        [](float value, int) -> String
+        {
+            String valueToText =
+                String(value, 1) + " ms";
+
+            return valueToText;
+        }))); // default
+
+
+    return layout;
+}
