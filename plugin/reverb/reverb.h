312d4d09c1580c2bcab89ab09399d01188a7b7b3
Michael Kauffmann <kauffmann.michael@gmail.com>
2026-04-22 10:12:33 +0200

    Initial commit

--- plugin/reverb/reverb.h
+++ plugin/reverb/reverb.h
@@ -39,7 +39,7 @@ END_JUCE_MODULE_DECLARATION
 
 #include "include/dsp/FDN_Reverb.h"
 #include "include/proces_DAW_control/Parameters.h"
-
+#include "CustomLookAndFeel.h"
 // order matters, e.g. if processor is not instantiated  before editor, as editor depend on it.
 #include "include/proces_DAW_control/PluginProcessor.h"
 #include "include/gui/Controller Slider.h"
